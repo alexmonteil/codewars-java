228e6b59d1b112aff4389189bf1abef5dabc8c6a
alexmonteil <47809218+alexmonteil@users.noreply.github.com>
2022-04-24 19:21:58 -0500

    Created FindShortestWord solution
    solved FindShortestWord in a String challenge

---
+++ FindShortestWord.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.List;
+
+/*
+
+Prompt :  Simple, given a string of words, return the length of the shortest word(s).
+String will never be empty and you do not need to account for different data types.
+
+*/
+
+public class FindShortestWord {
+
+    public static void main(String[] args) {
+        String testStr = "What could the shortest word be ?";
+        String shortest = FindShortest(testStr);
+        System.out.println("Should print ?");
+        assert shortest.equals("?");
+        System.out.println("The result is: " + shortest);
+    }
+
+    public static String FindShortest(String str) {
+        List<String> list = Arrays.asList(str.split("\\s"));
+        String shortest = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (shortest.length() > list.get(i).length()) {
+                shortest = list.get(i);
+            }
+        }
+        return shortest;
+    }
+}
