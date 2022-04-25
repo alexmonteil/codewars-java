30868aaca68dc91a5b6f0672360adad77a7d805f
alexmonteil <47809218+alexmonteil@users.noreply.github.com>
2022-04-25 01:43:33 -0500

    Updated requirements on challenges
    added requirements to each challenge.

--- StringEndsWith.java
+++ StringEndsWith.java
@@ -1,3 +1,7 @@
+/*
+    Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string). 
+ */
+
 public class StringEndsWith {
 
     public static void main(String[] args) {
